903e48090643a6e0a1cc092715f97fdeaf8ba6a9
cxy <2830206832@qq.com>
2023-12-23 16:31:17 +0800

    [修改]增加API接口

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -12,24 +12,34 @@ typedef struct dynamicArray
 
 /* 初始化 */
 int dynamicArrayInit(dyArray *pArray, int capacity);
+
 /* 插入 */
 int dynamicArrayInsert(dyArray *pArray, ELEMENTTYPE val);
+
 /* 指定位置插入 */
 int dynamicArrayInsertByPos(dyArray *pArray, int pos, ELEMENTTYPE val);
+
 /* 更改指定位置数据 */
 int ModifydynamicArrayByPos(dyArray *pArray, int pos, ELEMENTTYPE val);
+
 /* 删除数据 */
 int dynamicArrayDelElementype(dyArray *pArray);
+
 /* 删除指定位置数据 */
 int dynamicArrayDelByPos(dyArray *pArray, int pos);
+
 /* 删除指定元素 */
 int dynamicArrayDelByElementype(dyArray *pArray, ELEMENTTYPE val);
+
 /* 销毁 */
 int dynamicArrayDestory(dyArray *pArray);
+
 /* 获取大小 */
 int dynamicArrayGetSize(dyArray *pArray, int *pSize);
+
 /* 获取容量 */
 int dynamicArrayGetCapacity(dyArray *pArray, int *pCapacity);
+
 /* 指定位置的元素数据*/ 
 int dynamicArrayGetElementypeByPos(dyArray *pArray, int pos, ELEMENTTYPE *val);
 
