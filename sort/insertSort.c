83595447c8f0352200a420ea0bbde8000be2d44c
cxy <2830206832@qq.com>
2024-01-02 15:02:59 +0800

    [新增]插入排序

--- sort/insertSort.c
+++ sort/insertSort.c
@@ -3,12 +3,15 @@
 /* 插入，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
 因而在从后向前扫描过程中，需要反复把已排序元素逐步向后 */
 /* 交换两个整数 */
-void swap(int *val1, int *val2)
+
+void printArray(int *nums, int length)
 {
-    int temp = *val1;
-    *val1 = *val2;
-    *val2  = temp;
+     for(int idx = 0; idx < length; idx++)
+    {
+        printf("%d ", nums[idx]);
+    }
 }
+
 void insertSort(int *nums, int numsSize)
 {
     for(int idx = 1; idx < numsSize; idx++)
@@ -19,36 +22,37 @@ void insertSort(int *nums, int numsSize)
         {
             if(nums[jdx - 1] > temp)
             {
-                nums[jdx] = nums[jdx - 1];//大的数往后移
+                nums[jdx] = nums[jdx - 1];//大的数往后移   
             }
-            else
+            else// nums[jdx - 1] <= temp jdx== 0
             {
                 nums[jdx] = temp;
                 break;
             }
-        }
+        } 
     }
 }
 
-void insertion_sort(int arr[], int len){
-    int i,j,temp;
-    for (i=1;i<len;i++){
-            temp = arr[i];
-            for (j=i;j>0 && arr[j-1]>temp;j--)
-                    arr[j] = arr[j-1];
-            arr[j] = temp;
+void insertion_sort(int arr[], int len)
+{
+    int i, j, temp;
+    for (i = 1;i < len; i++)
+    {
+        temp = arr[i];
+        for (j = i; j > 0 && arr[j-1] > temp; j--)
+        {
+            arr[j] = arr[j-1];
+        }
+        arr[j] = temp;
     }
 }
 
 int main()
 {
-    int nums[BUFFER_SIZE] = {-1, 5, 3, 8, 1};
-    int size = sizeof(nums)/sizeof(nums[0]);
-    insertSort(nums, size);
-    for(int idx = 0; idx < BUFFER_SIZE; idx++)
-    {
-        printf("%d ", nums[idx]);
-    }
+    int nums[] = {0, 6, 5, 8, 1};
+    int length = sizeof(nums)/sizeof(nums[0]);
+    insertSort(nums, length);
+    printArray(nums, length);
     printf("\n");
     return 0;
 }
