148079907b47aa61fdb3543160a7043db11264f0
cxy <2830206832@qq.com>
2024-01-02 15:08:53 +0800

    [新增]插入排序注释

--- sort/insertSort.c
+++ sort/insertSort.c
@@ -2,7 +2,6 @@
 #define BUFFER_SIZE 6
 /* 插入，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
 因而在从后向前扫描过程中，需要反复把已排序元素逐步向后 */
-/* 交换两个整数 */
 
 void printArray(int *nums, int length)
 {
@@ -27,7 +26,7 @@ void insertSort(int *nums, int numsSize)
             else// nums[jdx - 1] <= temp jdx== 0
             {
                 nums[jdx] = temp;
-                break;
+                break;//找到比temp小的值中的最大值的位置插入就跳出，不继续循环
             }
         } 
     }
