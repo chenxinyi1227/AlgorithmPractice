9219d792548f8cf4646f1d5604e3437e61364a15
cxy <2830206832@qq.com>
2024-01-13 21:50:39 +0800

    [新增]力扣中等题

---
+++ leetcode/leetcode-part2/3-5.c
@@ -0,0 +1,21 @@
+/* 最长回文字符串 */
+
+/* 给你一个字符串 s，找到 s 中最长的回文子串。
+如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+
+示例 1：
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。 */
+#if 0
+class Solution {
+public:
+    string longestPalindrome(string s) {
+
+    }
+};
+#else
+char* longestPalindrome(char* s) {
+    
+}
+#endif
