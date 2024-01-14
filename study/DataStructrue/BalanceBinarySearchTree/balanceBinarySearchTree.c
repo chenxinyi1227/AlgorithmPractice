b4184361e03e4fd599be538a556c0533a20cf987
cxy <2830206832@qq.com>
2024-01-14 19:55:09 +0800

    [待完成]获取二叉树的高度

--- study/DataStructrue/BalanceBinarySearchTree/balanceBinarySearchTree.c
+++ study/DataStructrue/BalanceBinarySearchTree/balanceBinarySearchTree.c
@@ -359,7 +359,7 @@ int balanceBinarySearchTreeGetNodeSize(balanceBinarySearchTree *pAvltree, int *p
 /* 获取二叉树的高度:需要记录每一层的结点树->每一层的节点数技术完成，就是树的高度加1 */
 int balanceBinarySearchTreeGetHeight(balanceBinarySearchTree *pAvltree, int *pHeight)
 {
-
+    
 }
 
 /* 二叉树的删除 */
