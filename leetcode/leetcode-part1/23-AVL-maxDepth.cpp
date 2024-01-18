10433f07c974aba81c39e3230fb7da661120e461
cxy <2830206832@qq.com>
2024-01-18 16:22:07 +0800

    树的最大深度

--- leetcode/leetcode-part1/23-AVL-maxDepth.cpp
+++ leetcode/leetcode-part1/23-AVL-maxDepth.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 /* 最大深度 */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+
+        // 如果 root 为空，直接返回 0
+        if(root == NULL) return 0;
+
+        // 递归调用 maxDepth，求出当前节点的左子树的最大深度
+        int left = maxDepth(root->left);
+
+        // 递归调用 maxDepth，求出当前节点的右子树的最大深度
+        int right = maxDepth(root->right);
+
+        // 求出当前节点的左右子树中较大的值
+        int childMaxDepth = max(left,right);
+
+        // 二叉树的最大深度就是它的左右子树中较大的值加上 1
+        return childMaxDepth + 1;
+    }
+};
+
