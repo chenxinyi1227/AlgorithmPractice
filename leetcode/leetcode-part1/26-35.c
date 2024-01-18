34d515b73339aaa53c6342553e05217c668863ea
cxy <2830206832@qq.com>
2024-01-18 16:22:49 +0800

    搜索插入位置

--- leetcode/leetcode-part1/26-35.c
+++ leetcode/leetcode-part1/26-35.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 /* 搜索插入位置 */
+int searchInsert(int* nums, int numsSize, int target) 
+{
+     int left = 0;
+    int right = numsSize -1;
+    int a=numsSize;
+    while(left <= right)
+    {
+        int mid = left+(right-left)/2;
+        if (target <= nums[mid])
+        {
+            a = mid;
+            right = mid - 1;
+        } 
+        else 
+        {
+            left = mid + 1;
+        }
+    }
+    return a;
+}
