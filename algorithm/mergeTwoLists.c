90fccbbd0f8a02f0ed0c7b9acba941c748b47495
cxy <2830206832@qq.com>
2024-01-19 21:44:05 +0800

    整理文件

--- algorithm/mergeTwoLists.c
+++
@@ -1,6 +0,0 @@
-/* 合并两个有序链表 
-将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有结点组成的·*/
-struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) 
-{
-
-}
--- algorithm/string.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char str[]= "hello world";
-    printf("str:%s\n",str + 5);
-}
